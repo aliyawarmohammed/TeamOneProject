a6a2c2c6746c925cb588ccaec245ab3faab62bca
Dell <Dell@DESKTOP-3T7SP2U>
2024-06-04 21:31:50 +0530

    Committed

---
+++ TeamOneProject/ArrayListExample.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamOneProject
+{
+    internal class ArrayListExample
+    {
+
+        public static void Main()
+        {
+            ArrayList st = new ArrayList();
+           
+            ArrayList list= new ArrayList();
+            list.Add(1);
+            list.Add('C');
+            list.Add("Nitish");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+
+            }
+            Console.WriteLine("2nd loop");
+            Console.WriteLine(list.Contains('C'));
+            Console.WriteLine(list[2]);//accessing Elements
+
+            //list.Remove('C');value you can pass here
+            //list.RemoveAt(2);index 
+            //list.Insert(1, 15);
+            // list.Reverse();
+            //list.Clear();
+          //  st.AddRange(list);
+            //st.RemoveRange(2, 1);
+            //foreach (var item in st)
+            //{
+            //    Console.WriteLine(item);
+
+            //}
+            Console.ReadLine();
+        }
+
+    }
+}
+///ArrayList is use store Collection Deifferent Datatype Element were we can add
+///remove ,reverse ,insert  any element s from the array
---
+++ TeamOneProject/HashTableExample.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamOneProject
+{
+    internal class HashTableExample
+    {
+        public static void Main()
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add(1, "Jeans");
+            hashtable.Add(2, 8.9);
+
+            Console.WriteLine(hashtable[1]);
+            Console.WriteLine("-------");
+            //hashtable.Remove(1);
+            //Console.WriteLine(hashtable.ContainsValue("Jeans"));
+            hashtable[1] = "Shirt";
+            foreach (DictionaryEntry d in hashtable)
+            {
+                Console.WriteLine(d.Key+":"+d.Value);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
