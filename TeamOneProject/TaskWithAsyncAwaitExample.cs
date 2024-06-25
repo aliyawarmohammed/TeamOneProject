387d0fb9f823cb778f212a93461e1fe0eb1a71af
Dell <Dell@DESKTOP-3T7SP2U>
2024-06-25 21:51:10 +0530

    Committed

---
+++ TeamOneProject/TaskWithAsyncAwaitExample.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamOneProject
+{
+    internal class TaskWithAsyncAwaitExample
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Main Method Started...");
+            MySecondMethod();
+            Console.WriteLine("Main Method End...");
+            Console.ReadLine();
+
+
+        }
+
+        public async static void MySecondMethod()
+        {
+            Console.WriteLine("MySecondMethod Method Started...");
+            await Wait();
+            Console.WriteLine("MySecondMethod Method End...");
+        }
+
+        public static async Task Wait()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("10 Sec waitedd...");
+
+        }
+
+    }
+}
